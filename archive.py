83f7a02ae49e64765d70a206112bcf7688b8de73
dan_fred <dannyfrederick360@gmail.com>
2018-11-08 10:53:56 -0500

    --

--- archive.py
+++ archive.py
@@ -30,7 +30,8 @@ class Archive:
         # articles by year
         if temp == 0:
             x = ['1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2016', '2017', '2018']
-            temp1 = self.getAnswer('Which year?', x)
+            year = self.getAnswer('Which year?', x)
+            y = '/press/us/en/pressreleases/finder.wss?year={}'.format()
         # articles by category
         elif temp == 1:
             pass
