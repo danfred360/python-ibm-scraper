8e3a8a0b3a3d8bd39a6b880b07b1bb2eda0180eb
NinjaMan36036 <dannyfrederick360@gmail.com>
2018-11-29 17:36:12 -0500

    linked list

---
+++ linked-list.py
@@ -0,0 +1,44 @@
+class node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = node()
+
+    def append(self, data):
+        new_node = node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
+
+    def display(self):
+        elems = []
+        cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            elems.append(cur_node.data)
+        print(elems)
+
+    def get(self, index):
+        if index >= self.length():
+            print('Error: Index out of range!')
+            return None
+        cur_node = cur_node.next
+        if cur_idx == index: return cur_node.data
+        cur_idx += 1
+
+l = linked_list()
+l.append(1)
+l.append(2)
+l.display()
