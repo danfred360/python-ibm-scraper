7431fee2b13d8d84150b85b0ec34d85cca3022fe
NinjaMan36036 <dannyfrederick360@gmail.com>
2018-11-29 18:02:24 -0500

    --

---
+++ cls.py
@@ -0,0 +1,15 @@
+import os
+import time
+'''
+os.system('ipconfig')
+time.sleep(2)
+os.system('cls')
+
+time.sleep(2)
+
+os.system('ipconfig')
+time.sleep(2)
+os.system('cls')
+'''
+os.system('@echo off')
+os.system('ipconfig /all > V:network_info.txt')
--- linked-list.py
+++ linked-list.py
@@ -34,11 +34,36 @@ class linked_list:
         if index >= self.length():
             print('Error: Index out of range!')
             return None
-        cur_node = cur_node.next
-        if cur_idx == index: return cur_node.data
-        cur_idx += 1
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_idx == index: return cur_node.data
+            cur_idx += 1
+
+    def erase(self, index):
+        if index >= self.length():
+            print('Error: Erase index out of range!')
+            return
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_idx == index:
+                last_node.next = cur_node.next
+                return
+            cur_idx += 1
 
 l = linked_list()
 l.append(1)
 l.append(2)
+l.append(3)
+l.append(4)
+l.display()
+
+print('element at 2nd index: {}'.format(l.get(2)))
+
+l.erase(1)
+
 l.display()
